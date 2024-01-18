e35736c3292d2507865bd8432e47be9c60326525
manojgarnayak <manojgarnayak07@gmail.com>
2024-01-18 10:57:26 +0530

    Counter Project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () =>{
+    setCounter(counter + 1)
+  }
+  const removeValue = () =>{
+    if (counter > 0){
+      setCounter(counter - 1)  
+    }
+  }
+
+  return (
+    <>
+      <h1>Manoj Garnayak</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br/><br/>
+      <button onClick={removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
